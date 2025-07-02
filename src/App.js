a0090cca4b9af11457b2c8b9ebbdc51d2716f98c
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-07-02 02:08:03 -0700

    start

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-yellow-200 to-red-300">
       <h1 className="text-4xl font-bold text-black drop-shadow-lg">
-        🍲 Food Recipe App with TailwindCSS!
+        🍲 Food Recipe App with TaildldklwindCSS!
       </h1>
     </div>
+  
   );
 }
 
