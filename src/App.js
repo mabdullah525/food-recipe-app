980fe9f7584588201f5d19994e31d1f16c4d47ff
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-07-02 02:03:03 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-yellow-200 to-red-300">
+      <h1 className="text-4xl font-bold text-black drop-shadow-lg">
+        🍲 Food Recipe App with TailwindCSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
