0719e5abbd5e9682305de03b186f065b00108816
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-07-02 14:48:24 -0700

    Add state management and input handling for recipe search functionality

--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Mainpage = () => {
+    const [data, setData] = useState()
+    const [search, setSearch] = useState('')
+
+    const handleInput = (event) => {
+        setSearch(event.target.value)
+        
+    }
+
+
+    const myFun = async () => {
+        const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        const jsonData = await get.json()
+
+    }
     return (
         <div className='container'>
             <div className='search-bar'>
                 <input
                     type="text"
-                    placeholder='Search for recipes...'
+                    placeholder='Search for recipes...' onChange={handleInput}
                     className='input'
                 />
                 <button
-                    className='search-btn'
+                    className='search-btn' onClick={myFun}
                 >
                     Search
                 </button>
