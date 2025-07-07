9a935a9344ef7c4d3eabf02f5229051e0d69d892
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-07-07 03:00:28 -0700

    Refactor Mainpage and Mealinfo components for improved search handling and data display

--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -4,39 +4,50 @@ import Mealcards from './Mealcards'
 const Mainpage = () => {
   const [data, setData] = useState([])
   const [search, setSearch] = useState('')
+  const [msg, setMsg] = useState('')
 
   const handleInput = (event) => {
     setSearch(event.target.value)
   }
 
   const myFun = async () => {
-    const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-    const jsonData = await get.json()
-    console.log(jsonData)
-    setData(jsonData.meals || [])
+    if (search === '') {
+      setMsg('Please enter a search term')
+    } else {
+      const get = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+      const jsonData = await get.json()
+      // console.log(jsonData)
+      setData(jsonData.meals || [])
+
+    }
+
   }
 
   return (
-    <div className='container px-3'>
-      <div className='search-bar'>
-        <input
-          type="text"
-          placeholder='Search for recipes...'
-          onChange={handleInput}
-          className='input'
-        />
-        <button
-          onClick={myFun}
-          className='search-btn'
-        >
-          Search
-        </button>
-      </div>
+    <>
+      <h1 className='head'>FOOD RECIPE APP</h1>
+      <div className='container px-3'>
+        <div className='search-bar'>
+          <input
+            type="text"
+            placeholder='Search for recipes...'
+            onChange={handleInput}
+            className='input'
+          />
+          <button
+            onClick={myFun}
+            className='search-btn'
+          >
+            Search
+          </button>
+          <h4>{msg}</h4>
+        </div>
 
-      <div>
-        <Mealcards detail={data} />
+        <div>
+          <Mealcards detail={data} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/Components/Mealinfo.js
+++ src/Components/Mealinfo.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useState } from 'react';
 
 const Mealinfo = () => {
     const { mealid } = useParams();
+    const [info, setInfo] = useState();
 
     console.log(mealid);
 
     const getInfo = async () => {
         const get = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`);
         const jsonData = await get.json();
-        console.log(data);
-    }
-    if (mealid != "") {
+        console.log(jsonData.meals[0]);
+        setInfo(jsonData.meals[0]);
+    };
 
+    React.useEffect(() => {
+        getInfo();
+        // eslint-disable-next-line
+    }, [mealid]);
 
-    }
     return (
-        <div>Mealinfo</div>
+        !info ? (
+            <div>Data Not Found</div>
+        ) : (
+            <div className='mealinfo'>
+                <img src={info.strMealThumb} alt={info.strMeal} />
+                <div className='info'>
+                    <h1>Recipe Detail</h1>
+                    <button>{info.strMeal}</button>
+                    <h3>Instruction's</h3>
+                    <p>{info.strInstructions}</p>
+                </div>
+            </div>
+        )
     )
 }
 
